5a136c9e10f5034940fe143fb647a242840bf81e
Klaas Werkman <105836264+BKlaasWerkman@users.noreply.github.com>
2022-05-27 10:38:02 -0500

    Update karbs.sh

--- karbs.sh
+++ karbs.sh
@@ -14,7 +14,7 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit 1 ;;
 esac done
 
-[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/bklaaswerkman/voidrice.git"
+[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/bklaaswerkman/linux-dotfiles.git"
 [ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/BKlaasWerkman/KARBS/master/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
